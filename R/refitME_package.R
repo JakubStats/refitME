c86c30699e6b163ee5746a876ae3c457b96c38f0
Jakub <jakub.stoklosa@gmail.com>
2019-04-19 22:41:11 +1000

    Package updates - 19/04/2019

--- R/refitME_package.R
+++ R/refitME_package.R
@@ -92,9 +92,9 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     if(p1 == 2) X <- cbind(rep(1, B*n), X1_j, (X1_j)^2)
 
     if(!is.null(ncol(W1))){
-      if(d>2 && p1 == 2) X <- cbind(X, do.call(rbind, replicate(B, W1[, -c(1:p1)], simplify = FALSE)))
-      if(d>2 && p1 == 1) X <- cbind(X, do.call(rbind, replicate(B, W1[, -c(1:p1)], simplify = FALSE)))
-      if(d == 2 && p1 == 2) X <- X
+      if(d>2 & p1 == 2) X <- cbind(X, do.call(rbind, replicate(B, W1[, -c(1:p1)], simplify = FALSE)))
+      if(d>2 & p1 == 1) X <- cbind(X, do.call(rbind, replicate(B, W1[, -c(1:p1)], simplify = FALSE)))
+      if(d == 2 & p1 == 2) X <- X
     }
 
     X <- as.matrix(X)
@@ -106,7 +106,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     if(p1 == 2) colnames(X)[2:3] <- c("x1", "I(x1^2)")
 
     mu.e1 <- mean(X1_j)
-    }
+  }
 
   if(is.matrix(sigma.sq.u) == T){
     q1 <- dim(W)[2]
@@ -115,7 +115,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
       p <- rep(1, q1)
       col.nameX <- paste0('x', 1:q1)
       XA <- W
-      }
+    }
     if(d>q1){
       p <- c()
       col.nameX <- c()
@@ -124,28 +124,30 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
       kk1 <- 1
 
       for(kk in 1:q1){
-        if(sum((W[, kk])^2!=(W1[, kk1+1])) == n){
+        if(sum((W[, kk])^2 != (W1[, kk1+1])) == n){
           p1 <- 1
           kk1 <- kk
           col.nameX1 <- paste0('x', kk)
-          }
-       if(sum((W[, kk])^2 == (W1[, kk1+1])) == n){
-         p1 <- 2
-         kk1 <- kk+1
-         col.nameX1 <- c(paste0('x', kk), paste0(paste0('I(x', 1), '^2)'))
-         }
+        }
+
+        if(sum((W[, kk])^2 == (W1[, kk1+1])) == n){
+          p1 <- 2
+          kk1 <- kk + 1
+          col.nameX1 <- c(paste0('x', kk), paste0(paste0('I(x', 1), '^2)'))
+        }
 
         p <- c(p, p1)
         col.nameX <- c(col.nameX, col.nameX1)
       }
     }
+
     p2 <- sum(p)
     sigma.sq.u1 <- sigma.sq.u
     sigma.sq.e1 <- diag(sigma.sq.e)
 
     w1 <- W[, 1]
     U1_j <- stats::rnorm(n*B, 0, sd = sqrt(rep(sigma.sq.u1[1, 1], B)))
-    X1_j <- rep(w1, B)-U1_j
+    X1_j <- rep(w1, B) - U1_j
 
     X <- cbind(rep(1, B*n), X1_j)
     XA <- X[, 2]
@@ -156,7 +158,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     for(kk in 2:q1){
       w1 <- W[, kk]
       U1_j <- stats::rnorm(n*B, 0, sd = sqrt(rep(sigma.sq.u1[kk, kk], B)))
-      X1_j <- rep(w1, B)-U1_j
+      X1_j <- rep(w1, B) - U1_j
 
       XA <- cbind(XA, X1_j)
 
@@ -173,8 +175,8 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
 
     colnames(X) <- names(stats::coef(mod))
 
-    colnames(X)[2:(p2+1)] <- col.nameX
-    }
+    colnames(X)[2:(p2 + 1)] <- col.nameX
+  }
 
   while(cond){
     # MC and E-step.
@@ -185,7 +187,6 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     if(family == "gaussian") prY <- stats::dnorm(bigY, muPred, 1)
     if(family == "binomial") prY <- stats::dbinom(bigY, 1, muPred)
     if(family == "poisson") prY <- stats::dpois(bigY, muPred)
-    #if(family == "Gamma") prY <- stats::dgamma(bigY, shape = shape.est, rate = shape.est/muPred)
     if(family == "Gamma") prY <- stats::dgamma(bigY, shape = shape.est, scale = muPred/shape.est)
     if(family == "negbin") prY <- stats::dnbinom(bigY, size = theta.est, mu = muPred)
 
@@ -198,19 +199,18 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     weights1[is.nan(weights1)] <- 0
 
     if(family == "gaussian"){
-      mod <- stats::lm(bigY~X-1, weights = weights1)
+      mod <- stats::lm(bigY ~ X - 1, weights = weights1)
       sigma.sq.est <- (summary(mod)$sigma)^2
     }
-    if(family == "binomial") mod <- stats::glm(bigY~X-1, weights = weights1, family = "binomial")
-    if(family == "poisson") mod <- stats::glm(bigY~X-1, weights = weights1, family = "poisson")
-    if(family == "Gamma") mod <- stats::glm(bigY~X-1, weights = weights1, family = Gamma(link = "log"))
-    if(family == "negbin") mod <- MASS::glm.nb(bigY~X-1, weights = weights1, init.theta = theta.est)
+    if(family == "binomial") mod <- stats::glm(bigY ~ X - 1, weights = weights1, family = "binomial")
+    if(family == "poisson") mod <- stats::glm(bigY ~ X - 1, weights = weights1, family = "poisson")
+    if(family == "Gamma") mod <- stats::glm(bigY ~ X - 1, weights = weights1, family = Gamma(link = "log"))
+    if(family == "negbin") mod <- MASS::glm.nb(bigY ~ X - 1, weights = weights1, init.theta = theta.est)
 
     beta.update <- stats::coef(mod)
     if(family == "negbin") theta.update <- mod$theta
     muPred <- stats::predict(mod, type = "response")
     if(family == "Gamma"){
-      #shape.update <- MASS::gamma.shape(mod, it.lim = 10000, eps.max = 0.0000001, verbose = T)$alpha
       shape.update <- summary(mod)[14]$dispersion
     }
 
@@ -219,8 +219,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
       mu.e1.update <- stats::weighted.mean(X[, 2], w = weights1)
     }
 
-    if(is.matrix(sigma.sq.u) == T)
-      {
+    if(is.matrix(sigma.sq.u) == T){
       sigma.sq.e1.update <- c()
       mu.e1.update <- c()
 
@@ -242,15 +241,14 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     if(family == "Gamma") shape.norm <- sum((shape.est-shape.update)^2)
 
     if(is.matrix(sigma.sq.u) == F) diff.sig_e <- abs(sigma.sq.e1.update-sigma.sq.e1)
-    if(is.matrix(sigma.sq.u) == T) diff.sig_e <- sum(abs(diag(sigma.sq.e1.update)-diag(sigma.sq.e1)))
+    if(is.matrix(sigma.sq.u) == T) diff.sig_e <- sum(abs(diag(sigma.sq.e1.update) - diag(sigma.sq.e1)))
 
-    diff.mu_e <- sum((mu.e1.update-mu.e1)^2)
+    diff.mu_e <- sum((mu.e1.update - mu.e1)^2)
 
-    reps <- reps+1 # Keeps track of number of iterations.
+    reps <- reps + 1 # Keeps track of number of iterations.
 
     if(family == "binomial" | family == "poisson" | family == "gaussian"){
-      if(diff.mu_e<epsilon && diff.sig_e<epsilon && beta.norm<epsilon)
-        {
+      if(diff.mu_e<epsilon & diff.sig_e<epsilon & beta.norm<epsilon){
         cond <- FALSE
         print("convergence :-)")
         print(reps)
@@ -259,7 +257,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     }
 
     if(family == "negbin"){
-      if(diff.mu_e<epsilon && diff.sig_e<epsilon && beta.norm<epsilon && theta.norm<epsilon){
+      if(diff.mu_e<epsilon & diff.sig_e<epsilon & beta.norm<epsilon & theta.norm<epsilon){
         cond <- FALSE
         print("convergence :-)")
         print(reps)
@@ -268,7 +266,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     }
 
     if(family == "Gamma"){
-      if(diff.mu_e<epsilon && diff.sig_e<epsilon && beta.norm<epsilon && shape.norm<epsilon){
+      if(diff.mu_e<epsilon & diff.sig_e<epsilon & beta.norm<epsilon & shape.norm<epsilon){
         cond <- FALSE
         print("convergence :-)")
         print(reps)
@@ -295,7 +293,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
 
   # Standard error calculations start here.
 
-  beta.est.se1 <- sqrt(diag(stats::vcov(mod))) # Naive SE estimator.
+  beta.est.se1 <- sqrt(diag(stats::vcov(mod)))  # Naive SE estimator.
 
   if(family == "gaussian") beta.est.se1 <- sqrt(diag(stats::vcov(mod)*B))
 
@@ -319,7 +317,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     sand1[is.nan(sand1)] <- 1
     SS_1 <- t(sandwich::estfun(mod)*B)%*%sand1
     u.bar <- solve(stats::vcov(mod)*B)
-    beta.est.se2 <- sqrt(diag(solve(u.bar-SS_1/B^2+S_1/B^2)))
+    beta.est.se2 <- sqrt(diag(solve(u.bar-SS_1/B^2 + S_1/B^2)))
   }
 
   if(family!="gaussian"){
@@ -360,6 +358,7 @@ MCEMfit_glm <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
 MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsilon = 0.00001, theta.est = 1, shape.est = 10){
 
   options(warn = -1)
+
   reps <- 0
   cond <- TRUE
 
@@ -386,7 +385,7 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     sigma.sq.e1 <- sigma.sq.e
 
     U1_j <- stats::rnorm(n*B, 0, sd = sqrt(rep(sigma.sq.u1, B)))
-    X1_j <- rep(w1, B)-U1_j
+    X1_j <- rep(w1, B) - U1_j
 
     mu.e1 <- mean(X1_j)
 
@@ -397,7 +396,7 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
 
     colnames(dat_new)[2] <- "x1"
 
-    form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1)+.-s(w1)))
+    form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1) +. - s(w1)))
 
     if(d>1){
       smooth.err <- which(W1[1, ]%in%w1[1])
@@ -430,18 +429,18 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     sigma.sq.e1 <- diag(sigma.sq.e)
     w1 <- W[, 1]
     U1_j <- stats::rnorm(n*B, 0, sd = sqrt(rep(sigma.sq.u1[1, 1], B)))
-    X1_j <- rep(w1, B)-U1_j
+    X1_j <- rep(w1, B) - U1_j
     X <- cbind(X1_j)
     mu.e1 <- mean(X1_j)
 
     for(kk in 2:q1){
       w1 <- W[, kk]
       U1_j <- stats::rnorm(n*B, 0, sd = sqrt(rep(sigma.sq.u1[kk, kk], B)))
-      X1_j <- rep(w1, B)-U1_j
+      X1_j <- rep(w1, B) - U1_j
 
       X <- cbind(X, X1_j)
       mu.e1 <- c(mu.e1, mean(X1_j))
-      }
+    }
 
     dat_new <- cbind(bigY, X)
 
@@ -460,10 +459,10 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
       }
     }
 
-    if(q1 == 2) form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1)+s(x2)+.-s(w1)-s(w2)))
-    if(q1 == 3) form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1)+s(x2)+s(x3)+.-s(w1)-s(w2)-s(w3)))
-    if(q1 == 4) form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1)+s(x2)+s(x3)+s(x4)+.-s(w1)-s(w2)-s(w3)-s(w4)))
-    if(q1 == 5) form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1)+s(x2)+s(x3)+s(x4)+s(x5)+.-s(w1)-s(w2)-s(w3)-s(w4)-s(w5)))
+    if(q1 == 2) form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1) + s(x2) +. - s(w1) - s(w2)))
+    if(q1 == 3) form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1) + s(x2) + s(x3) +. - s(w1) - s(w2) - s(w3)))
+    if(q1 == 4) form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1) + s(x2) + s(x3) + s(x4) +. - s(w1) - s(w2) - s(w3) - s(w4)))
+    if(q1 == 5) form.name <- stats::as.formula(stats::update(stats::formula(mod), bigY ~ s(x1) + s(x2) + s(x3) + s(x4) + s(x5) +. - s(w1) - s(w2) - s(w3) - s(w4) - s(w5)))
     if(q1>5) print("Sorry, refitME cannot handle more than 5 error-contaminated varaibles with GAM!")
   }
 
@@ -495,7 +494,7 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     if(family == "gaussian"){
       mod <- mgcv::gam(formula = form.name, weights = weights1, data = dat_new)
       sigma.sq.est <- (summary(mod)$dispersion)
-      }
+    }
     if(family == "binomial") mod <- mgcv::gam(formula = form.name, weights = weights1, family = "binomial", gamma = 1.4, data = dat_new)
     if(family == "poisson") mod <- mgcv::gam(formula = form.name, weights = weights1, family = "poisson", data = dat_new)
     if(family == "Gamma") mod <- mgcv::gam(formula = form.name, weights = weights1, family = Gamma(link = "log"), data = dat_new)
@@ -509,7 +508,7 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     if(is.matrix(sigma.sq.u) == F){
       sigma.sq.e1.update <- SDMTools::wt.var(X[, 1], w = weights1)
       mu.e1.update <- stats::weighted.mean(X[, 1], w = weights1)
-      }
+    }
 
     if(is.matrix(sigma.sq.u) == T){
       sigma.sq.e1.update <- c()
@@ -521,25 +520,25 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
 
         mu.e1.update1 <- stats::weighted.mean(X[, kk], w = weights1)
         mu.e1.update <- c(mu.e1.update1, mu.e1.update)
-        }
-      sigma.sq.e1.update <- diag(sigma.sq.e1.update)
       }
+      sigma.sq.e1.update <- diag(sigma.sq.e1.update)
+    }
 
     # Convergence monitoring.
 
-    beta.norm <- sum((beta.est-beta.update)^2)
+    beta.norm <- sum((beta.est - beta.update)^2)
     if(family == "negbin") theta.norm <- sum((theta.est-theta.update)^2)
     if(family == "Gamma") shape.norm <- sum((shape.est-shape.update)^2)
 
-    if(is.matrix(sigma.sq.u) == F) diff.sig_e <- abs(sigma.sq.e1.update-sigma.sq.e1)
-    if(is.matrix(sigma.sq.u) == T) diff.sig_e <- sum(abs(diag(sigma.sq.e1.update)-diag(sigma.sq.e1)))
+    if(is.matrix(sigma.sq.u) == F) diff.sig_e <- abs(sigma.sq.e1.update - sigma.sq.e1)
+    if(is.matrix(sigma.sq.u) == T) diff.sig_e <- sum(abs(diag(sigma.sq.e1.update) - diag(sigma.sq.e1)))
 
-    diff.mu_e <- sum((mu.e1.update-mu.e1)^2)
+    diff.mu_e <- sum((mu.e1.update - mu.e1)^2)
 
     reps <- reps+1 # Keeps track of number of iterations.
 
     if(family == "binomial" | family == "poisson" | family == "gaussian"){
-      if((diff.mu_e<epsilon && diff.sig_e<epsilon && beta.norm<epsilon) | reps>50){
+      if((diff.mu_e<epsilon & diff.sig_e<epsilon & beta.norm<epsilon) | reps>50){
         cond <- FALSE
         print("convergence :-)")
         print(reps)
@@ -548,7 +547,7 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     }
 
     if(family == "negbin"){
-      if((diff.mu_e<epsilon && diff.sig_e<epsilon && beta.norm<epsilon && theta.norm<epsilon) | reps>50){
+      if((diff.mu_e<epsilon & diff.sig_e<epsilon & beta.norm<epsilon & theta.norm<epsilon) | reps>50){
         cond <- FALSE
         print("convergence :-)")
         print(reps)
@@ -557,7 +556,7 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
     }
 
     if(family == "Gamma"){
-      if((diff.mu_e<epsilon && diff.sig_e<epsilon && beta.norm<epsilon && shape.norm<epsilon) | reps>50){
+      if((diff.mu_e<epsilon & diff.sig_e<epsilon & beta.norm<epsilon & shape.norm<epsilon) | reps>50){
         cond <- FALSE
         print("convergence :-)")
         print(reps)
@@ -585,7 +584,7 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
   # Standard error calculations start here.
 
   beta.est <- stats::coef(mod)
-  beta.est.se1 <- sqrt(diag(stats::vcov(mod))) # Naive SE estimator.
+  beta.est.se1 <- sqrt(diag(stats::vcov(mod)))   # Naive SE estimator.
 
   if(family == "gaussian") beta.est.se1 <- sqrt(diag(stats::vcov(mod)*B))
 
@@ -603,13 +602,13 @@ MCEMfit_gam <- function(mod, family, sigma.sq.u, W, sigma.sq.e = 1, B = 50, epsi
 
   for(ii in 1:n){
     index_vec <- ind_mat[, ii]
-    S_1 <- S_1+(apply(estfun_mat[index_vec, ], 2, sum))%*%t(apply(estfun_mat[index_vec, ], 2, sum))
-    }
+    S_1 <- S_1 + (apply(estfun_mat[index_vec, ], 2, sum))%*%t(apply(estfun_mat[index_vec, ], 2, sum))
+  }
 
   if(family == "gaussian"){
     u.bar <- solve(stats::vcov(mod)*B)
-    beta.est.se2 <- sqrt(diag(solve(u.bar-SS_1/B^2+S_1/B^2)))
-    }
+    beta.est.se2 <- sqrt(diag(solve(u.bar-SS_1/B^2 + S_1/B^2)))
+  }
 
   if(family == "binomial" | family == "poisson" | family == "Gamma" | family == "negbin"){
     sand1 <- (sandwich::estfun(mod)/mod$prior)
@@ -645,8 +644,8 @@ refitME <- function(mod, sigma.sq.u, W, B = 50, epsilon = 0.00001){
 
   if(is.matrix(sigma.sq.u) == F){
     print("One specified error-contaminated covariate.")
-    sigma.sq.e <- stats::var(mod$model[, 2])-sigma.sq.u
-    }
+    sigma.sq.e <- stats::var(mod$model[, 2]) - sigma.sq.u
+  }
 
   if(is.matrix(sigma.sq.u) == T){
     print("Multiple specified error-contaminated covariates.")
@@ -654,7 +653,7 @@ refitME <- function(mod, sigma.sq.u, W, B = 50, epsilon = 0.00001){
     sigma.sq.e <- c()
 
     for(kk in 1:q1){
-      sigma.sq.e1 <- stats::var(W[, kk])-sigma.sq.u[kk, kk]
+      sigma.sq.e1 <- stats::var(W[, kk]) - sigma.sq.u[kk, kk]
       sigma.sq.e <- c(sigma.sq.e, sigma.sq.e1)
     }
   }
@@ -669,8 +668,8 @@ refitME <- function(mod, sigma.sq.u, W, B = 50, epsilon = 0.00001){
   }
 
   if(ob.type == "gam"){
-  family <- mod$family$family
-  if(strsplit(family, NULL)[[1]][1] == "N") family <- "negbin"
-  return(MCEMfit_gam(mod, family, sigma.sq.u, W, sigma.sq.e, B, epsilon))
+    family <- mod$family$family
+   if(strsplit(family, NULL)[[1]][1] == "N") family <- "negbin"
+   return(MCEMfit_gam(mod, family, sigma.sq.u, W, sigma.sq.e, B, epsilon))
   }
 }
